7a233211bc896419ae37935245f530326b83c530
PATADELLUISILLO <87156323+PATADELLUISILLO@users.noreply.github.com>
2021-07-08 21:01:50 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,13 +2,12 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  var firebaseConfig = {
-    apiKey: "AIzaSyBGSG2AK_z6AEE0nRQlIMfNRjQeOcmx5dg",
+ apiKey: "AIzaSyBGSG2AK_z6AEE0nRQlIMfNRjQeOcmx5dg",
     authDomain: "autenfia41.firebaseapp.com",
     projectId: "autenfia41",
     storageBucket: "autenfia41.appspot.com",
     messagingSenderId: "5630037742",
     appId: "1:5630037742:web:840f7dfb68e99b7478adff",
     measurementId: "G-932N1VJ8RF"
-  };
+  });
 

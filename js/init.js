874c4ba48decf2b71c1f7ab37feef1a100b29043
PATADELLUISILLO <87156323+PATADELLUISILLO@users.noreply.github.com>
2021-07-17 21:31:15 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyB4fLoWMRkEv82nmJzV7JQWpd9pN2GGotg",
+    authDomain: "chatlfgv.firebaseapp.com",
+    projectId: "chatlfgv",
+    storageBucket: "chatlfgv.appspot.com",
+    messagingSenderId: "844488727802",
+    appId: "1:844488727802:web:12c5473a099500334d3d43",
+    measurementId: "G-KW4ZJ1XE5V"
+    });
